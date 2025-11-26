e8d52999e21fe7717d83d912f95c2ad63661ad29
Samarth Gupta <samarth1234gupta@gmail.com>
2025-11-26 23:56:12 +0530

    Fixed Trouble keybinds not working

--- lua/samarth/plugins/trouble.lua
+++ lua/samarth/plugins/trouble.lua
@@ -1,27 +1,23 @@
 return {
-	{
-		"folke/trouble.nvim",
-		cmd = "Trouble",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
+    {
+        "folke/trouble.nvim",
+        cmd = "Trouble",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
 
-		opts = {
-			position = "bottom",
-			height = 12,
-			mode = "document_diagnostics",
-			auto_open = false,
-			auto_close = true,
-			use_diagnostic_signs = true,
-		},
+        opts = {
+            position = "bottom",
+            height = 12,
+            auto_open = false,
+            auto_close = true,
+            use_diagnostic_signs = true,
+        },
 
-		keys = {
-			keys = {
-				{ "<leader>xx", "<cmd>Trouble diagnostics<cr>",         desc = "Diagnostics (Workspace)" },
-				{ "<leader>xd", "<cmd>Trouble document_diagnostics<cr>", desc = "Diagnostics (File)" },
-				{ "<leader>xt", "<cmd>Trouble todo<cr>",                desc = "TODOs" },
-				{ "<leader>xX", "<cmd>Trouble diagnostics toggle<cr>",  desc = "Toggle Trouble" },
-			}
-
-		},
-	}
+        keys = {
+            { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics" },
+            { "<leader>xd", "<cmd>Trouble document_diagnostics toggle<cr>", desc = "Document Diagnostics" },
+            { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "TODOs" },
+            { "<leader>xX", "<cmd>Trouble diagnostics toggle<cr>", desc = "Toggle Trouble" },
+        },
+    }
 }
 
